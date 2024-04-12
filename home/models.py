f0f3038468db6deae899aff3de97506e2f994e37
mohamed <mderkaoui10@gmail.com>
2024-04-12 11:04:37 +0200

    add chagings database

--- home/models.py
+++ home/models.py
@@ -2,6 +2,36 @@ from django.db import models
 
 from wagtail.models import Page
 
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 class HomePage(Page):
-    pass
+    """The home page of the site."""
+    
+    intro = models.CharField(max_length=255, blank=True)  # Define the 'intro' field here
+    tittle = models.CharField(max_length=255, blank=True)  # Define the 'intro' field here
+    body = RichTextField(blank=True)
+    
+    
+    template = "home/home_page.html"
+
+    def get_tittle(self):
+        return self.tittle
+
+    def get_intro(self):
+        return self.intro
+
+    def get_context(self, request):
+        # Update context to include only published sections
+        context = super().get_context(request)
+        context['sections'] = self.get_children().live()
+        return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('intro'),  # Include the 'intro' field in the content panels
+    ]
+
+    class Meta:
+        verbose_name = 'home page'
+        verbose_name_plural = 'home pages'
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./indam/static/ /**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
