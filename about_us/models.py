ce1ccc14525086ee36df0f7a97e9f75ddcb23c5b
mohamed <mderkaoui10@gmail.com>
2024-04-26 19:53:40 +0200

    "Fix typo in email field in FormPage model[/HUMAN]

--- about_us/models.py
+++ about_us/models.py
@@ -2,3 +2,49 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+# import MultiFieldPanel:
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+
+class abouta_usPage(Page):
+
+    body = RichTextField(blank=True)
+    template = 'about_us/about_us.html'
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
+    def get_context(self, request):
+        """
+        A method to retrieve the context with respect to the given request.
+        Parameters:
+            request: The request object to retrieve context for.
+        Returns:
+            The context obtained after calling the superclass's get_context method.
+        """
+        context = super().get_context(request)
+        return context
+    class ourteamMember(models.Model):
+        name = models.CharField(max_length=255)
+        role = models.CharField(max_length=255)
+        image = models.ForeignKey(
+            "wagtailimages.Image",
+            null=True,
+            blank=True,
+            on_delete=models.SET_NULL,
+            related_name="+",
+        )
+
+        panels = [
+            FieldPanel('name'),
+            FieldPanel('role'),
+            FieldPanel('image'),
+        ]
+
+        def __str__(self):
+            return self.name
+        
--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,38 @@
 from django.db import models
+
+# import parentalKey:
 from modelcluster.fields import ParentalKey
+
+# import FieldRowPanel and InlinePanel:
 from wagtail.admin.panels import (
-    FieldPanel, FieldRowPanel,
-    InlinePanel, MultiFieldPanel
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    PublishingPanel,
 )
+
 from wagtail.fields import RichTextField
+from wagtail.models import (
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+)
+
+# import AbstractEmailForm and AbstractFormField:
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
+# import FormSubmissionsPanel:
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
+from wagtail.snippets.models import register_snippet
 
+
+# ... keep the definition of NavigationSettings and FooterText. Add FormField and FormPage:
 class FormField(AbstractFormField):
     page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
 
@@ -17,17 +42,19 @@ class FormPage(AbstractEmailForm):
     thank_you_text = RichTextField(blank=True)
 
     content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
         FieldPanel('intro'),
         InlinePanel('form_fields', label="Form fields"),
         FieldPanel('thank_you_text'),
         MultiFieldPanel([
             FieldRowPanel([
-                FieldPanel('from_address', classname="col6"),
-                FieldPanel('to_address', classname="col6"),
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
             ]),
             FieldPanel('subject'),
         ], "Email"),
     ]
-
-from wagtail.contrib.forms.panels import FormSubmissionsPanel
+    
+    template = 'contact/form_page.html'
+    form_page_landing_template =  'contact/form_page_landing.html'
 
