dacbf7e702bdd14f59522812a1e46792e356d739
mohamed <mderkaoui10@gmail.com>
2024-05-07 19:31:16 +0200

    d

--- bhh.bat
+++ bhh.bat
@@ -1,6 +1,11 @@
-python manage.py dumpdata --natural-foreign --indent 2     -e/
-contenttypes -e auth.permission     -e/
-wagtailcore.groupcollectionpermission     -e/
-wagtailcore.grouppagepermission -e wagtailimages.rendition     -e/
-wagtailsearch.indexentry -e wagtailsearch.sqliteftsindexentry     -e/
-sessions > politica.json
+python -Xutf8 manage.py dumpdata --natural-foreign --indent 2 --exclude=wagtailcore.Site -o db.json
+ mv db.sqlite3 db.sqlite3.old
+py manage.py migrate
+python manage.py shell -c "from django.core.management.sql import sql_flush; from django.db import connection; print(sql_flush(connection, ['wagtailcore_site'], interactive=False));"
+ python manage.py loaddata db.json
+if [ $? -eq 0 ]; then
+    mv db.sqlite3 db1.sqlite3
+
+    mv db.sqlite3.old db.sqlite3 
+fi
+ mv db.sqlite3.old db.sqlite3 
